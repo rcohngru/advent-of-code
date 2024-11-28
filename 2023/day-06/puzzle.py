d5d8d0b760e3d4e390c6b760fb0d92dc8d1ffd77
Rafi Cohn-Gruenwald <rafael.cohngruenwald@gmail.com>
2024-11-28 10:57:51 -0800

    solve day 6

--- 2023/day-06/puzzle.py
+++ 2023/day-06/puzzle.py
@@ -6,13 +6,33 @@ def main():
 
 
 def part_1():
+  lines = ingest_file("input.txt")
+  times = map(int, lines[0].split()[1:])
+  distances = map(int, lines[1].split()[1:])
+  moe = 1
+  for t, d in zip(times, distances):
+    poss = 0
+    for i in range(t):
+      dist = i * (t - i)
+      if dist > d:
+        poss += 1
 
-  sol = f""
+    moe *= poss
+
+
+  sol = f"{moe}"
   print(f"The solution for Part 1 is: {sol}")
 
 def part_2():
-
-  sol = f""
+  lines = ingest_file("input.txt")
+  time = int("".join(lines[0].split()[1:]))
+  distance = int("".join(lines[1].split()[1:]))
+  poss = 0
+  for i in range(time):
+    dist = i * (time - i)
+    if dist > distance:
+      poss += 1
+  sol = f"{poss}"
   print(f"The solution for Part 2 is: {sol}")
 
 def ingest_file(file):
